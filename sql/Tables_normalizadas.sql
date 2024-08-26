20c04ccc1ae0459fda518932a2355ed3cc822d72
23dereek <168207899+DerekNoguera@users.noreply.github.com>
2024-08-26 16:03:22 -0600

    cambios

--- sql/Tables_normalizadas.sql
+++ sql/Tables_normalizadas.sql
@@ -3,13 +3,16 @@ drop Database BaseDatos;
 use BaseDatos;
 
 
-
+# Una tabla con la direccion de usuarios para enviarsela a Usuarios es necesaria si quieres 
+# Hacerla 3FN
 Create Table Direccion_Usuarios(
     DireccionID INT PRIMARY KEY AUTO_INCREMENT,
     Calle Varchar(100),
     Ciudad Varchar(50),
     Estado Varchar(50)
 );
+
+#Recibe una llave foranea de la tabla Direccion_Usuarios y se crea la tabla usuarios
 create table Usuarios(
     UsuarioID int primary key AUTO_INCREMENT,
     Nombre varchar(50),
@@ -21,12 +24,17 @@ create table Usuarios(
 
 
 
+
+#Crea la tabla Hotel_Direccion para  que son las direcciones de los hoteles y se le dan
+# como llaves foraneas a Hoteles
 Create table Hotel_Direccion(
     direccion_id INT PRIMARY KEY AUTO_INCREMENT,
     Ciudad Varchar(50),
     Estado Varchar(50),
     Codigo_Postal varchar(10)
 );
+
+#Crea Hoteles y recibe datos como llave foranea de Hotel_direccion y es una nueva tabla que almacena datos de los hoteles
 create Table Hoteles(
     HotelID int primary key AUTO_INCREMENT,
     Nombre Varchar(50),
@@ -38,10 +46,25 @@ create Table Hoteles(
 
 
 
+
+# Crea estados de ocupado, disponible o reservado donde, 1 - Disponible, 2 - Reserved y 
+# 3 - Ocupado
+Create Table Estado(
+    EstadoID int PRIMARY KEY AUTO_INCREMENT,
+    Descripcion Varchar(100)
+);
+
+
+#crea tipos de habitaciones Suit, Individual, doble y familiar
 Create Table Tipo_Habitacion(
     TipoID int PRIMARY KEY AUTO_INCREMENT,
     NombreTipo VARCHAR(50) NOT NULL
 );
+
+
+#Habitaciones es una tabla que tiene los datos de la habitacion y recibe como llave foranea el esto de si es Disponible,
+#Ocupado o reservado, tambien recibe el ID de el hotel de la tabla Hoteles para saber que hotel es, y recibe Tipo de habitaciones
+#Coomo llave foranea de la tabla tipo de habitacions para saber si es SUit, Matrimonial ETC, y tambien se crean otros datos
 create Table Habitaciones(
     HabitacionID int PRIMARY KEY AUTO_INCREMENT,
     HotelID int,
@@ -50,12 +73,18 @@ create Table Habitaciones(
     Precio_Noche Decimal(10,2),
     Descripcion TEXT,
     Capacidad INT,
+    EstadoID INT,
+    Foreign Key (EstadoID) REFERENCES Estado(EstadoID),
     Foreign Key (HotelID) REFERENCES Hoteles(HotelID),
     Foreign Key (Tipo_habitacion) REFERENCES Tipo_Habitacion(TipoID)
 );
 
 
 
+
+
+#Crea una tabla para las reservas pero la insercion de datos de esta tabla se hacen con los Store Procedure de el archivo 
+#Store_procedure que inserta datos y tambien puede eliminarlos por ID
 create Table Reservas(
     ReservaID int primary Key auto_increment,
     UsuarioID int,
@@ -68,20 +97,3 @@ create Table Reservas(
 );
 
 
-
-
-
-
-
--- Consultas SQL (Queries):
--- ○ Consulta para obtener los hoteles con mayor número de reservas.
--- ○ Consulta para contar cuántas habitaciones disponibles hay en un hotel específico en una
--- fecha dada.
--- ○ Consulta para buscar hoteles por nombre.
--- ○ Consulta para buscar hoteles cuya ubicación comienza con un texto específico.
--- ○ Consulta para buscar hoteles cuya ubicación termina con un texto específico.
--- ○ Consulta para obtener las reservas de un cliente (por email) realizadas en el mes anterior.
--- ○ Consulta para calcular el promedio de reservas diarias en un hotel.
--- ○ Consulta para identificar el hotel con la mayor ocupación en el mes anterior.
--- ○ Consulta para listar los hoteles que tienen habitaciones disponibles pero no han sido
--- reservadas en el último mes.
