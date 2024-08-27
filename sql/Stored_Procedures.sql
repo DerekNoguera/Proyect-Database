e9ce5f0dae02dc5b377102edafad740924158ffd
victoria0506 <vicguti527@gmail.com>
2024-08-27 11:55:36 -0600

    Realizacion de Vistas, Indices, Consultas

---
+++ sql/Stored_Procedures.sql
@@ -0,0 +1,12 @@
+
+DELIMITER //
+CREATE PROCEDURE Cancelar_Reserva(
+    IN p_ReservaID INT
+)
+BEGIN
+DELETE FROM Reservas WHERE ReservaID = p_ReservaID
+END
+DELIMITER;
+
+CALL Cancelar_Reserva()
+
